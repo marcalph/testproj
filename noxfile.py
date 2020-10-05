d59055ef046f0667ac51c90b1f58e9329d42a0e4
marcalph <marcalph@protonmail.com>
2020-10-05 22:54:38 +0200

    lint

--- noxfile.py
+++ noxfile.py
@@ -3,3 +3,11 @@ import nox
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+
+locations = "src", "tests", "noxfile.py"
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
